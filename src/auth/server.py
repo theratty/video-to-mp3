a91c2a66812f4cb651f41f593956cc33c35ad654
Adrian Wysocki <adrian.wyso.97@gmail.com>
2023-11-03 21:37:38 +0100

    add auth service

---
+++ src/auth/server.py
@@ -0,0 +1,69 @@
+import jwt, datetime, os
+from flask import Flask, request
+from flask_mysqldb import MySQL
+
+
+server = Flask(__name__)
+myqsl = MySQL(server)
+
+# config
+
+server.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
+server.config['MYSQL_PORT'] = os.environ.get('MYSQL_PORT')
+server.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
+server.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
+server.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
+
+@server.route('/login', methods=['GET'])
+def login():
+    auth = request.authorization
+    if not auth:
+        return "missing credentials", 401
+
+    # check db
+    cur = myqsl.connection.cursor()
+    res = cur.execute(
+        f'SELECT email, password FROM user WHERE email={auth.username}'
+    )
+
+    if res > 0:
+        user_row = cur.fetchone()
+        email = user_row[0]
+        password = user_row[1]
+
+        if auth.username != email and auth.password != password:
+            return "invalid credentials", 401
+        else:
+            return createJWT(auth.username, os.environ.get('JWT_SECRET'), True)
+    else:
+        return "invalid credentials", 401
+
+@server.route('/validate', methods=['POST'])
+def validate():
+    encoded_jwt = request.headers['Authorization']
+    if not encoded_jwt:
+        return "missing authorization token", 401
+
+    encoded_jwt = encoded_jwt.split(' ')[1]
+
+    try:
+        decoded = jwt.decode(encoded_jwt, os.environ.get('JWT_SECRET'), algorithms=['HS256'])
+    except:
+        return "not authorized", 403
+    
+    return decoded, 200
+
+def createJWT(username, secret, authz):
+    return jwt.encode(
+        {
+            "username": username,
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(days=1),
+            "iat": datetime.datetime.utcnow(),
+            "admin": authz
+        },
+        secret,
+        algorithm='HS256'
+    )
+
+if __name__ == '__main__':
+    server.run(host='0.0.0.0', port=5000)
